0702e0ec2383ccaee05b4f099e75bf217d9126c8
00DZnkbl00 <dawidzdrojewski12345678@gmail.com>
2024-11-22 23:38:32 +0100

    Mini Bug-Fix źle podpiety css

--- front/project help desk/src/pages/SignUp/SignUp.jsx
+++ front/project help desk/src/pages/SignUp/SignUp.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios"; // Dodano import axios
-import "./Signup.css";
+import "./SignUp.css";
 
 const Signup = () => {
   const navigate = useNavigate();
